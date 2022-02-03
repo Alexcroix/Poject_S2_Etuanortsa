20f9da9d366c76d5c18bd0c66f7ffe2123b8a768
Alexcroix <97702688+Alexcroix@users.noreply.github.com>
2022-02-04 00:17:24 +0100

    Sync player
    Fin syncronisation player si possible

--- Etuanortsa/Assets/Scripts/Player/PlayerMovement.cs
+++ Etuanortsa/Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 
 public class PlayerMovement : MonoBehaviour
@@ -13,6 +14,8 @@ public class PlayerMovement : MonoBehaviour
     public float mouse_y;
     public bool moving;
     public Camera cam;
+    public GameObject PlayerCamera;
+    PhotonView View;
 
     //Player
     public Rigidbody2D player;
@@ -24,55 +27,75 @@ public class PlayerMovement : MonoBehaviour
     public Sprite Right_weapon;
 
     public float MovementSpeed = 10f;
-    
+
+    void Start()
+    {
+        View = GetComponent<PhotonView>();
+        if (View.IsMine)
+        {
+            PlayerCamera.SetActive(true);
+        }
+    }
+
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (View.IsMine)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
 
-        mousePos = cam.ScreenToViewportPoint(Input.mousePosition);
+            mousePos = cam.ScreenToViewportPoint(Input.mousePosition);
 
-        mouse_x = (mousePos.x - 0.5f);
-        mouse_y = (mousePos.y - 0.5f);
-        moving = movement != Vector2.zero;
+            mouse_x = (mousePos.x - 0.5f);
+            mouse_y = (mousePos.y - 0.5f);
+            moving = movement != Vector2.zero;
+        }
     }
 
     private void FixedUpdate()
     {
-        player.MovePosition(player.position + movement * MovementSpeed * Time.deltaTime);
-        weapon.MovePosition(player.position + movement * MovementSpeed * Time.deltaTime);
-
-        Vector2 loorDir = mousePos;
-        loorDir.x = loorDir.x - 0.5f;
-        loorDir.y = loorDir.y - 0.5f;
-        float angle = Mathf.Atan2(loorDir.y, loorDir.x) * Mathf.Rad2Deg - 180f;
-        weapon.rotation = angle;
-
-        animator.SetFloat("mouse_x", mouse_x);
-        animator.SetFloat("mouse_y", mouse_y);
-        animator.SetBool("moving", moving);
-
-        tempPos = weapon.transform.position;
-
-        if (loorDir.y > 0)
+        if (View.IsMine)
         {
-            tempPos.z = -4;
-        }
-        else
-        {
-            tempPos.z = -6;
-        }
+            //VectorMovement
+            player.MovePosition(player.position + movement * MovementSpeed * Time.deltaTime);
+            weapon.MovePosition(player.position + movement * MovementSpeed * Time.deltaTime);
 
-        if (loorDir.x <= 0)
-        {
-            weapon.GetComponent<SpriteRenderer>().sprite = Left_weapon;
-        }
-        else
-        {
-            weapon.GetComponent<SpriteRenderer>().sprite = Right_weapon;
-        }
+            //WeaponAim
+            Vector2 loorDir = mousePos;
+            loorDir.x = loorDir.x - 0.5f;
+            loorDir.y = loorDir.y - 0.5f;
+            float angle = Mathf.Atan2(loorDir.y, loorDir.x) * Mathf.Rad2Deg - 180f;
+            weapon.rotation = angle;
 
-        weapon.transform.position = tempPos;
+            //SetValueAnimator
+            animator.SetFloat("mouse_x", mouse_x);
+            animator.SetFloat("mouse_y", mouse_y);
+            animator.SetBool("moving", moving);
+
+            //SpiteWeapon
+            tempPos = weapon.transform.position;
+
+            if (loorDir.y > 0)
+            {
+                tempPos.z = -4;
+            }
+            else
+            {
+                tempPos.z = -6;
+            }
+
+            if (loorDir.x <= 0)
+            {
+                weapon.GetComponent<SpriteRenderer>().sprite = Left_weapon;
+            }
+            else
+            {
+                weapon.GetComponent<SpriteRenderer>().sprite = Right_weapon;
+            }
+
+            weapon.transform.position = tempPos;
+        }
+        
     }
 }
 
