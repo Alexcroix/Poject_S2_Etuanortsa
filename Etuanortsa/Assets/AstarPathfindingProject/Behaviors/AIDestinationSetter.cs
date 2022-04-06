6d01331a33704f64e29ae486c751ec5194532a79
eclv <97709407+eclv@users.noreply.github.com>
2022-04-06 19:55:33 +0200

    ia
    ia qui suit joueur

--- Etuanortsa/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
+++ Etuanortsa/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -1,5 +1,9 @@
 using UnityEngine;
 using System.Collections;
+<<<<<<< Updated upstream
+using System;
+=======
+>>>>>>> Stashed changes
 
 namespace Pathfinding {
 	/// <summary>
@@ -16,8 +20,33 @@ namespace Pathfinding {
 	public class AIDestinationSetter : VersionedMonoBehaviour {
 		/// <summary>The object that the AI should move to</summary>
 		public Transform target;
+<<<<<<< Updated upstream
+		IAstarAI ai;
+		public GameObject[] PlayerList;// a suprimer plus tard pour mettre la vraie liste
+		public GameObject min;
+		public float hitboxmob;
+		public float hitboxplayer; // on doit le supprimer plus tard (il faut recuperer le hit box du joueur il est le meme pour tt les personnes)
+		public float test= 0;
+
+
+		void Start()
+		{
+			min = PlayerList[1];
+
+		}
+		
+		
+=======
+		public Transform target2;
+		public Transform target3;
+		public Transform target4;
+		public int distance1;
+		public int distance2;
+		public int distance3;
+		public int distance4;
 		IAstarAI ai;
 
+>>>>>>> Stashed changes
 		void OnEnable () {
 			ai = GetComponent<IAstarAI>();
 			// Update the destination right before searching for a path as well.
@@ -30,10 +59,96 @@ namespace Pathfinding {
 		void OnDisable () {
 			if (ai != null) ai.onSearchPath -= Update;
 		}
+<<<<<<< Updated upstream
+		
+		/// <summary>Updates the AI's destination every frame</summary>
+		void Update () {
+			int nbjoueur = PlayerList.Length;
+			bool f = ai.reachedEndOfPath;
+
+			for (int i = 0; i< nbjoueur; i++)
+            {
+				if (cvt(this.gameObject, PlayerList[i]) < cvt(this.gameObject, min))
+                {
+					
+					min = PlayerList[i];
+				}
+				
+			}
+			ai.destination = min.transform.position;
+
+			if (cvt(this.gameObject,min)<= hitboxmob + hitboxplayer)
+            {
+				// mob attaque
+				// animation de l attaque
+				ai.canMove = false;
+			
+			}
+
+			if (cvt(this.gameObject, min) > hitboxmob + hitboxplayer)
+			{
+				ai.canMove = true;
+			}
+
+
+
+
+
+
+			//float distance1 = Vector3.Distance(target.position, mob.position);
+			//float distance2 = Vector3.Distance(target2.position, mob.position);
+			//float distance3 = Vector3.Distance(target3.position, mob.position);
+			//float distance4 = Vector3.Distance(target4.position, mob.position);
+
+			//if (target != null && ai != null && distance1<= distance2 && distance1<= distance3 && distance1<= distance4)
+			//{ 
+			//	ai.destination = target.position; 
+			//}
+			//if (target2 != null && ai != null && distance2 < distance1 && distance2 <= distance3 && distance2 <= distance4)
+			//{ 	
+			//	ai.destination = target2.position;
+			//}
+			//if (target3 != null && ai != null && distance3 < distance1 && distance3 < distance2 && distance3 <= distance4)
+			//{
+			//	ai.destination = target3.position;
+			//}
+			//if (target4 != null && ai != null && distance4 < distance1 && distance4 < distance2 && distance4 < distance3)
+			//{
+			//	ai.destination = target4.position;
+			//}
+		}
+		public float cvt(GameObject ia, GameObject mob)
+		{
+			return Math.Abs (mob.transform.position.x - ia.transform.position.x) + Math.Abs(mob.transform.position.y - ia.transform.position.y);
+		}
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position, hitboxmob);
+		}
+
+=======
 
 		/// <summary>Updates the AI's destination every frame</summary>
 		void Update () {
-			if (target != null && ai != null) ai.destination = target.position;
+			
+			if (target != null && ai != null && distance1<= distance2 && distance1<= distance3 && distance1<= distance4)
+			{ 
+				ai.destination = target.position; 
+			}
+			if (target2 != null && ai != null && distance2 < distance1 && distance2 <= distance3 && distance2 <= distance4)
+            { 	
+				ai.destination = target2.position;
+			}
+			if (target3 != null && ai != null && distance3 < distance1 && distance3 < distance2 && distance3 <= distance4)
+			{
+				ai.destination = target3.position;
+			}
+			if (target4 != null && ai != null && distance4 < distance1 && distance4 < distance2 && distance4 < distance3)
+			{
+				ai.destination = target4.position;
+			}
 		}
+>>>>>>> Stashed changes
 	}
 }
---
+++ Etuanortsa/Assets/Scripts/Mobs/IA.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+using System;
+
+public class IA : MonoBehaviour
+{
+
+    public AIPath aiPath;
+    public Animator animator2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator2.SetBool("X", true);
+        animator2.SetBool("Y", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+       
+
+
+
+
+
+
+    }
+
+}
