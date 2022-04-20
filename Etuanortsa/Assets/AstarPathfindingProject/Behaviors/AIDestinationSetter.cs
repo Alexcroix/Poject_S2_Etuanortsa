3fa1b9d67370a76584f83c2fc30b43cdb4e4907a
eclv <97709407+eclv@users.noreply.github.com>
2022-04-20 14:49:13 +0200

    ia base version 2
    animation du mob ne marche pas, ia creer et spawn dans les colider aussi

---
+++ Etuanortsa/Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Pathfinding {
+	/// <summary>
+	/// Sets the destination of an AI to the position of a specified object.
+	/// This component should be attached to a GameObject together with a movement script such as AIPath, RichAI or AILerp.
+	/// This component will then make the AI move towards the <see cref="target"/> set on this component.
+	///
+	/// See: <see cref="Pathfinding.IAstarAI.destination"/>
+	///
+	/// [Open online documentation to see images]
+	/// </summary>
+	[UniqueComponent(tag = "ai.destination")]
+	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
+	public class AIDestinationSetter : VersionedMonoBehaviour {
+		/// <summary>The object that the AI should move to</summary>
+		public Transform target;
+		public GameObject[] PlayerList;// a suprimer plus tard pour mettre la vraie liste
+		private GameObject min;
+		public float hitboxmob;
+		public float hitboxplayer; // on doit le supprimer plus tard (il faut recuperer le hit box du joueur il est le meme pour tt les personnes)
+		public Rigidbody2D rigidbody;
+		
+		IAstarAI ai;
+
+		void OnEnable () {
+			ai = GetComponent<IAstarAI>();
+			// Update the destination right before searching for a path as well.
+			// This is enough in theory, but this script will also update the destination every
+			// frame as the destination is used for debugging and may be used for other things by other
+			// scripts as well. So it makes sense that it is up to date every frame.
+			if (ai != null) ai.onSearchPath += Update;
+		}
+
+		void OnDisable () {
+			if (ai != null) ai.onSearchPath -= Update;
+		}
+		void Start()
+		{
+			min = PlayerList[0];
+			ai.canMove = true;
+
+		}
+
+		/// <summary>Updates the AI's destination every frame</summary>
+		void Update () {
+			int nbjoueur = PlayerList.Length;
+			bool f = ai.reachedEndOfPath;
+
+			for (int i = 0; i < nbjoueur; i++)
+			{
+				if (cvt(this.gameObject, PlayerList[i]) < cvt(this.gameObject, min))
+				{
+
+					min = PlayerList[i];
+				}
+
+			}
+			ai.destination = min.transform.position;
+
+			
+		}
+
+
+		public float cvt(GameObject ia, GameObject mob)
+		{
+			return Math.Abs(mob.transform.position.x - ia.transform.position.x) + Math.Abs(mob.transform.position.y - ia.transform.position.y);
+		}
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.green;
+			Gizmos.DrawWireSphere(transform.position, hitboxmob);
+		}
+	}
+}
---
+++ Etuanortsa/Assets/Scripts/spawn_enemy/spawn_enemy1.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_enemy1 : MonoBehaviour
+{
+    public GameObject enemy;
+    public Collider2D col;
+    public float Xpos = 0;
+    public float Ypos = 0;
+    public int nbenemy;
+    public Vector2 pointspawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        new WaitForSeconds(10f);
+        StartCoroutine(EnemyDrop());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator EnemyDrop()
+    {
+        for (int i = 0; i < nbenemy; i++)
+        {
+            Xpos = Random.Range(1, 50);
+            Ypos = Random.Range(1, 50);
+            pointspawn.x = Xpos;
+            pointspawn.y = Ypos;
+            while (!col.OverlapPoint(pointspawn))
+            {
+                Xpos = Random.Range(1, 50);
+                Ypos = Random.Range(1, 50);
+                pointspawn.x = Xpos;
+                pointspawn.y = Ypos;
+            }
+            Instantiate(enemy, pointspawn, Quaternion.identity);
+            yield return new WaitForSeconds(1f);
+
+        }
+    }
+}
